9838f9014ca00e3d96a11207c89d69de344bfd80
abdullah <iaiabidii@gmail.com>
2025-07-15 22:02:59 +0300

    chore: extract environment variables into config/env.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,7 +1,7 @@
 export const env = {
-  BASE_URL: process.env.NEXT_PUBLIC_API_URL!,
+  BASE_URL: process.env.NEXT_PUBLIC_API_URL || "",
 };
 
-if (!env.BASE_URL) {
+if (!env.BASE_URL && process.env.NODE_ENV === "production") {
   throw new Error("❌ Missing NEXT_PUBLIC_API_URL in environment variables");
 }
