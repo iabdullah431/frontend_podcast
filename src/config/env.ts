b9b994dc6c35ba3be541777ec47e36d6dfae6d81
abdullah <iaiabidii@gmail.com>
2026-01-11 19:34:17 +0300

    change link

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const apiPodcast = axios.create({
   baseURL:
     process.env.NEXT_PUBLIC_API_URL ||
-    "https://backendpodcast-production.up.railway.app",
+    "https://podcast-app-euyh.vercel.app",
   headers: { "Content-Type": "application/json" },
 });
 
